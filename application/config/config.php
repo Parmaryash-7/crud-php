9dab100f16823256ed517a46f361c8ade0974ddc
Parmaryash-7 <parmars4679@gmail.com>
2025-05-20 19:54:27 +0530

    localhost

--- application/config/config.php
+++ application/config/config.php
@@ -24,8 +24,8 @@ defined('BASEPATH') or exit('No direct script access allowed');
 |
 */
 // $config['base_url'] = '';
-$config['base_url'] = 'http://192.168.1.15/crud-php/';
-// $config['base_url'] = 'http://localhost/crud-php/';
+// $config['base_url'] = 'http://192.168.1.15/crud-php/';
+$config['base_url'] = 'http://localhost/crud-php/';
 
 
 
